a1402034f5273accb2389d36114d2914773deaf8
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-12 11:35:29 -0600

    very cool

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :not_true_facts
+
+    # Custom Validations
+    def not_true_facts
+        if title == "True Facts"
+            errors.add(:title, "Not a valid title")
+        end
+    end
 end
