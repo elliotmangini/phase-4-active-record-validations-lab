b2d124e863db2e5980a57c1654e05f19085fd3e0
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-13 11:12:50 -0600

    commit tree 75f332c3157769f39a21c3666c66505d7fd14585

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,18 @@ class Post < ApplicationRecord
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: ["Fiction", "Non-Fiction"]
 
-    validate :not_true_facts
+    # validate :not_true_facts
+    validate :clickbait
 
     # Custom Validations
     def not_true_facts
         if title == "True Facts"
+            errors.add(:title, "Not a valid title")
+        end
+    end
+
+    def clickbait
+        if (!(title.include("Won't Believe") || title.include(title.include("Won't Believe") || (title.include("Guess")))
             errors.add(:title, "Not a valid title")
         end
     end
